3394aaa7a0886c26798070bc4179a6f0a858439b
Aayan sharma <aayansharma0887@gmail.com>
2025-12-12 09:22:32 +0530

    Add files via upload

---
+++ pasa.js
@@ -0,0 +1,27 @@
+
+    function validateForm() {
+        var user = document.getElementById("username").value;
+        var pass = document.getElementById("password").value;
+        var error = document.getElementById("error");
+
+        // Validation
+        if (user === "" || pass === "") {
+            error.textContent = "Please fill all fields";
+            return;
+        }
+
+        // Your custom login credentials
+        if (user === "admin" && pass === "12345") {
+            error.style.color = "#00ff88";
+            error.textContent = "Login successful! Redirecting...";
+
+            // Redirect to Instagram
+            setTimeout(function() {
+                window.location.href = "https://www.instagram.com/aayan.i0?igsh=MTVwNG52Z2pwMTExMA==";
+            }, 1200);
+
+        } else {
+            error.style.color = "#ff3b3b";
+            error.textContent = "Invalid username or password";
+        }
+    }
